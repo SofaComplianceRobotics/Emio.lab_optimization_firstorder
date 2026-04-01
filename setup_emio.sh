e4fee7c7261575a6f532a55d4b6bbcfcdc88802f
HanaeRateau <hanaerateau@yahoo.fr>
2026-04-01 12:01:59 +0200

    Deletes unecessary files again

--- setup_emio.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-mv ~/emio-labs/v25.12.01/assets/labs/labsConfig.json ~/emio-labs/v25.12.01/assets/labs/labsConfig.json.bkp
-ln -fs "$(realpath labsConfig.json)" ~/emio-labs/v25.12.01/assets/labs/
-ln -fs "$(realpath .)" ~/emio-labs/v25.12.01/assets/labs/
